860cfa18460b53b2d4045efa7665302a40eb1411
Wonjun Kang <michael.wonjun.kang@gmail.com>
2023-03-28 22:57:37 -0500

    add static bar-chart with data

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 import config from './src/config.js';
+
+function generateData(size) {
+  const data = Array(Number(size))
+    .fill(null)
+    .map((_, i) => i + 1);
+
+  const cellWidth = config.width / size;
+
+  const layoutData = data.map((el, idx) => {
+    return {
+      value: el,
+      width: cellWidth,
+      height: (config.height / size) * el,
+    };
+  });
+
+  return layoutData;
+}
+
+function update(dataset) {
+  d3.select('#chart')
+    .selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', (d) => d.width - 2)
+    .attr('height', (d) => d.height)
+    .attr('x', (d, idx) => d.width * idx)
+    .attr('y', (d) => config.height - d.height)
+    .attr('fill', 'blue');
+}
+
+const dataset = generateData(document.querySelector('.menu__size').value);
+update(dataset);
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  width: 860,
+  height: 860,
+  margin: 16,
+  transitionDuration: 250,
+  transitionDelay: 50,
+};
+
+export default config;
